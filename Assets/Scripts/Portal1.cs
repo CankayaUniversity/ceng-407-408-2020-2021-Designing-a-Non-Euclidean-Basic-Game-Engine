2aebd0378d3dc9e182544bcdc193d9264300775e
SMARTDANGER <obiemc298@gmail.com>
2021-05-15 22:04:12 +0300

    NONEUC PORTALS
    portalların çıkış yönü düzeltildi

--- Assets/Scripts/Portal1.cs
+++ Assets/Scripts/Portal1.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class Portal1 : MonoBehaviour
 {
     //Vector3(1f, 0f, 0f); == BLUE SIDE
     //Vector3(-1f, 0f, 0f); == PINK SIDE
     public GameObject conPortal;
 
 
-    void OnTriggerEnter(Collider c){
+    void OnTriggerEnter(Collider c)
+    {
         GameObject target = c.gameObject;
         Teleport(target);
     }
 
-    void Teleport(GameObject target){
+    void Teleport(GameObject target)
+    {
         Vector3 diff = new Vector3(1f, 0f, 0f);
         target.transform.position = conPortal.transform.position + diff;
-       // target.transform.rotation = conPortal.transform.rotation;
+        // target.transform.rotation = conPortal.transform.rotation;
     }
 }
