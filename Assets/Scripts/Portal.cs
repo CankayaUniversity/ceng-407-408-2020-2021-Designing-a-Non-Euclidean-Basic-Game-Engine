ae2260bc6f77f54c47b0d086bf12686526af8638
barismert98 <barismert98@gmail.com>
2021-06-12 17:36:16 +0300

    Demo

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -15,6 +15,7 @@ public class Portal : MonoBehaviour
 
     void Teleport(GameObject target){
         target.transform.localPosition = conPortal.transform.localPosition - transform.right * target.transform.localScale.x - transform.up * conPortal.transform.localScale.y/1.99f;
+        target.transform.Rotate(Vector3.up, 180);
         float scaling = conPortal.transform.localScale.y/transform.localScale.y; // Scale difference between portals
         target.transform.localScale = target.transform.localScale * scaling; // Scale object passing according to scale difference
     }
